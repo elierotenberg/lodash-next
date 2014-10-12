b2d366c2b6aade7b870d30358ef5dc1a094be92b
Elie Rotenberg <elie@rotenberg.io>
2014-10-12 23:17:30 +0200

    Added hash and secureHash.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
-require("regenerator/runtime");
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var plumber = require("gulp-plumber");
-var jshint = require("gulp-jshint");
-var stylish = require("jshint-stylish");
-var esnext = require("gulp-esnext");
-var del = require("del");
-var _ = require("lodash");
+require('regenerator/runtime');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var esnext = require('gulp-esnext');
+var del = require('del');
+var _ = require('lodash');
 
 var compile = function() {
-    return gulp.src("src/**/*.js")
-    .pipe(plumber())
-    .pipe(esnext())
-    .pipe(gulp.dest("dist"));
+  return gulp.src('src/**/*.js')
+  .pipe(plumber())
+  .pipe(esnext())
+  .pipe(gulp.dest('dist'));
 };
 
 var lint = function() {
-    return gulp.src("src/**/*.js")
-    .pipe(plumber())
-    .pipe(jshint({
-        esnext: true,
-        globals: {
-            Promise: true,
-        },
-    }))
-    .pipe(jshint.reporter(stylish));
+  return gulp.src('src/**/*.js')
+  .pipe(plumber())
+  .pipe(jshint({
+    esnext: true,
+    globals: {
+      Promise: true,
+    },
+  }))
+  .pipe(jshint.reporter(stylish));
 };
 
 var clean = function() {
-    del(["dist"]);
+  del(['dist']);
 };
 
-gulp.task("lint", lint);
-gulp.task("compile", ["lint"], compile);
+gulp.task('lint', lint);
+gulp.task('compile', ['lint'], compile);
 
-gulp.task("default", ["compile"]);
-gulp.task("clean", clean);
+gulp.task('default', ['compile']);
+gulp.task('clean', clean);
--- src/index.js
+++ src/index.js
@@ -6,104 +6,118 @@ var jsonpatch = require('jsonpatch');
 require('regenerator/runtime');
 
 _.mixin({
-    scope(fn, ctx) {
-        return function() {
-            fn.apply(ctx, arguments);
-        };
-    },
+  scope(fn, ctx) {
+    return function() {
+      fn.apply(ctx, arguments);
+    };
+  },
 
-    scopeAll(fn, ctx, methods) {
-        _.each(methods, function(method) {
-            ctx[method] = _.scope(ctx[method], ctx);
-        });
-    },
+  scopeAll(fn, ctx, methods) {
+    _.each(methods, function(method) {
+      ctx[method] = _.scope(ctx[method], ctx);
+    });
+  },
 
-    dev(fn) {
-        if(node.env.NODE_ENV === 'development') {
-            return fn();
-        }
-    },
+  dev(fn) {
+    if(node.env.NODE_ENV === 'development') {
+      return fn();
+    }
+  },
 
-    prod(fn) {
-        if(node.env.NODE_ENV === 'production') {
-            return fn();
-        }
-    },
+  prod(fn) {
+    if(node.env.NODE_ENV === 'production') {
+      return fn();
+    }
+  },
 
-    Promise: Promise,
+  Promise: Promise,
 
-    co: co,
+  co: co,
 
-    sha256: sha256,
+  sha256: sha256,
 
-    diff(prev, next) {
-        return jsonpatch.compare(prev, next);
-    },
+  hash(data) {
+    var a = 1;
+    var b = 0;
+    for (var i = 0; i < data.length; i++) {
+      a = (a + data.charCodeAt(i)) % MOD;
+      b = (b + a) % MOD;
+    }
+    return a | (b << 16);
+  },
 
-    patch(prev, diff) {
-        return jsonpatch.apply(prev, diff);
-    },
+  secureHash(data) {
+    return _.sha256(data);
+  },
 
-    base64Encode(s) {
-        return new Buffer(s).toString('base64');
-    },
+  diff(prev, next) {
+    return jsonpatch.compare(prev, next);
+  },
 
-    base64Decode(s) {
-        return new Buffer(s, 'base64').toString('utf-8');
-    },
+  patch(prev, diff) {
+    return jsonpatch.apply(prev, diff);
+  },
 
-    guid(prefix) {
-        var s4 = () => Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        prefix = prefix || '';
-        return `${prefix}${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-    },
+  base64Encode(s) {
+    return new Buffer(s).toString('base64');
+  },
 
-    breakpoint() {
-        /*jshint ignore:start*/
-        debugger;
-        /*jshint ignore:end*/
-    },
+  base64Decode(s) {
+    return new Buffer(s, 'base64').toString('utf-8');
+  },
 
-    rethrow(err, desc) {
-        if(err) {
-            if(desc) {
-                err = _.extendError(err, desc);
-            }
-            throw err;
-        }
-    },
+  guid(prefix) {
+    var s4 = () => Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+    prefix = prefix || '';
+    return `${prefix}${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+  },
 
-    extendError(err, desc) {
-        err.message = `${desc}: ${err.message}`;
-        return err;
-    },
+  breakpoint() {
+    /*jshint ignore:start*/
+    debugger;
+    /*jshint ignore:end*/
+  },
 
-    record(key, val) {
-        /*jshint ignore:start*/
-        return { [key]: val };
-        /*jshint ignore:end*/
-    },
+  rethrow(err, desc) {
+    if(err) {
+      if(desc) {
+        err = _.extendError(err, desc);
+      }
+      throw err;
+    }
+  },
 
-    asyncNoop: function*() {
-        yield _.defer;
-        return;
-    },
+  extendError(err, desc) {
+    err.message = `${desc}: ${err.message}`;
+    return err;
+  },
 
-    asyncTimeout: function*(delay) {
-        var timeout = function() {
-            return function(fn) {
-                setTimeout(fn, delay);
-            };
-        };
-        yield timeout();
-    },
+  record(key, val) {
+    /*jshint ignore:start*/
+    return { [key]: val };
+    /*jshint ignore:end*/
+  },
 
-    asyncConstant: function*(val) {
-        yield _.asyncNoop();
-        return val;
-    },
+  asyncNoop: function*() {
+    yield _.defer;
+    return;
+  },
+
+  asyncTimeout: function*(delay) {
+    var timeout = function() {
+      return function(fn) {
+        setTimeout(fn, delay);
+      };
+    };
+    yield timeout();
+  },
+
+  asyncConstant: function*(val) {
+    yield _.asyncNoop();
+    return val;
+  },
 
 });
 
