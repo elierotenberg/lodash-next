506511df94572bc5f1b342235955f8e80e85b547
Elie Rotenberg <elie@rotenberg.io>
2014-11-23 16:46:48 +0100

    Upgrade faulty gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ function build() {
   .pipe(plumber())
   .pipe(insert.prepend(
     'require(\'6to5/polyfill\'); ' +
+    'const Promise = require(\'bluebird\'); ' +
     'const __DEV__ = (process.env.NODE_ENV !== \'production\'); ' +
     'const __PROD__ = !__DEV__; ' +
     'const __BROWSER__ = (typeof window === \'object\'); ' +
